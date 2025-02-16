dec4911b4997853b571f6583a7bc66969ae16113
chittaranjan27 <kchittaranjandora@gmail.com>
2025-02-16 23:09:48 +0530

    updated file

--- Frontend/src/components/ChatContainer.jsx
+++ Frontend/src/components/ChatContainer.jsx
@@ -4,7 +4,7 @@ import { useEffect, useRef } from "react";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
-import { useAuthStore } from "../store/UseAuthStore";
+import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
